d5470678ef6359e49240d1f24be55b954ec90c04
User <User@DESKTOP-0TCDSR6>
2025-04-07 16:34:32 +0300

    Final

--- 11-JQuery/script.js
+++ 11-JQuery/script.js
@@ -1,4 +1,4 @@
-// JavaScript source code
+﻿// JavaScript source code
 $(document).ready(
 	function ()
 	{
@@ -82,6 +82,67 @@ $(document).ready(
 			{
 				$(this).hide();
 			}
-			)
+		)
+		///////////////////////////////////////////////
+		$('#input-data').click(
+			function ()
+			{
+				$(this).text("Регистрация");
+				$(this).html(`<em>${$(this).text()}</em>`)
+			}
+		);
+		$('#show-hide').click
+		(
+			function ()
+			{
+				let text = $('#show-hide').text();
+				$('#show-hide').text(text === 'Показать' ? 'Скрыть' : 'Показать');
+				text === 'Показать' ? $('ul').show() : $('ul').hide();
+			}
+		);
+
+		$('form').submit
+			(
+				function ()
+				{
+					//alert(`Привет ${$('input type:text').value()}, we are glad to CU`);
+					alert('Данные отправлены на Сервер');
+					alert(`Привет ${$(this).find('[type=text]').val()}, we are glad to CU, проветрите Ваш E-mail: ${$('[type=email]').val()}`);
+					$(this).html(`<h4>Привет ${$('[type=text]').val()}, we are glad to CU</h4>`);
+				}
+		);
+		/////////////////////////////////////////////////////////////////////
+		//$('form').after('<div>После регистрации проверьте Вашу почту<div>');
+		//$('form').remove();
+
+		$('#enable-bs').click
+			(
+				function ()
+				{
+					let class_list = 'row m-3';
+					let text = $(this).text();
+					//alert(text);
+					$(this).text(text === 'Включить Bootstrap' ? 'Выключить Bootstrap' : 'Включить Bootstrap')
+					text === 'Включить Bootstrap' ? $('[type]').addClass(class_list) : $('[type]').removeClass(class_list);
+				}
+			);
+		$('#switch-light').click
+			(
+				function ()
+				{
+					let text = $(this).text();
+					//alert(text);
+					if (text === 'Выключить свет')
+					{
+						$(this).text('Включить свет');
+						$('form').css({ "color":"lightgrey","background-color":"#323232"});
+					}
+					else
+					{
+						$(this).text('Выключить свет');
+						$('form').css({ "color": "black", "background-color": "white" });
+					}
+				}
+			);
 	}
 );
